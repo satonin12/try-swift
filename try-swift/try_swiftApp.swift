478242151927b93d4752aab5d65ed24bc3106ca6
satonin12 <vlad.satonin74@gmail.com>
2022-11-21 22:06:50 +0500

    Initial Commit

---
+++ try-swift/try_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  try_swiftApp.swift
+//  try-swift
+//
+//  Created by vsatonin on 21.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct try_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
